bbd624102972089f2dbb76014b2acca360a5ad59
batepanch <114772251+batepanch@users.noreply.github.com>
2022-10-04 14:59:02 +0400

    Add files via upload

---
+++ FifthTask.java
@@ -0,0 +1,12 @@
+package firstProject;
+
+public class FifthTask {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 3;
+		int y = 2;
+		int z = 2 * x + (y - 2) * 5;
+		System.out.println(z);
+	}
+}
---
+++ Second.java
@@ -0,0 +1,19 @@
+package firstProject;
+
+public class Second {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("P");
+		System.out.println("\t" +"A");
+		System.out.println("N");
+		System.out.println("\t" +"C");
+		System.out.println("H");
+		System.out.println("\t" +"E");
+		System.out.println("N");
+		System.out.println("\t" +"K");
+		System.out.println("O");
+		System.out.println("\t" +"V");
+	}
+
+}
